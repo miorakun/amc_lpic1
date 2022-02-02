623613bc55d83757e455eaac16b7a568515b0192
Parrot Live user <user@localhost.localdomain>
2022-02-02 19:48:16 +0000

    create start func

--- amc.py
+++ amc.py
@@ -13,3 +13,22 @@ global ch8_ch8
 global ch9_ch9
 global ch10_ch10
 
+
+# define for start write chalenges func name is start
+def start(): # start function
+    # try except for err handleing
+    try:
+        print("hello my friend this is amicheh's game you havehere for learning ")
+        na = input("enter your name ~$")
+        os.system("clear")
+        print("hello my friend this is amicheh's game you have here for learning ")
+        print ("In the first $amc1304$ challenge of 2022 you have to understand lpic1 you will have 10 questions and you will get a score at the end")
+        st = input("are you redy? [Y/n]")
+        if st == "y":
+            os.system("clear")
+            print ("welcome to amc1304")
+            print ("theme is Company")
+            print ("%s You are employed by a company and you have to meet their demands" % na)
+            ch1()
+    except:
+        print ("please run again")
