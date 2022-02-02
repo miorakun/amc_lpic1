9454c8009c2768acc37eb66b22cdbfdc6fe1013d
Parrot Live user <user@localhost.localdomain>
2022-02-02 19:47:12 +0000

    import and set globals

---
+++ amc.py
@@ -0,0 +1,15 @@
+import random 
+import os
+import hashlib 
+from time import sleep
+
+global md5
+global ch3_ch3 
+global ch4_ch4
+global ch5_ch5
+global ch6_ch6
+global ch7_ch7
+global ch8_ch8
+global ch9_ch9
+global ch10_ch10
+
