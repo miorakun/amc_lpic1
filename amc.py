4e2151b76ae2e15a00f3f351b402a9f5484f8a73
yaser amir chehrazi <92441512+amicheh@users.noreply.github.com>
2022-02-02 20:32:41 +0000

    Update amc.py

--- amc.py
+++ amc.py
@@ -4,15 +4,6 @@ import hashlib
 from time import sleep
 
 global md5
-global ch3_ch3 
-global ch4_ch4
-global ch5_ch5
-global ch6_ch6
-global ch7_ch7
-global ch8_ch8
-global ch9_ch9
-global ch10_ch10
-
 
 # define for start write chalenges func name is start
 def start(): # start function
